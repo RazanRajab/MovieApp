3c0ec465095ae19d378422ab2cb45f3945c128d1
Razan Rajab <MY_NAME@example.com>
2019-08-24 22:16:44 +0300

    add the settings option menu

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,20 +1,44 @@
 package com.example.movieapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.GridView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.mainGrid) GridView gridView;
+    @BindView(R.id.mainRecyclerView)RecyclerView RecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setTitle("Movies");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.most_popular:
+                break;
+            case R.id.top_rated:
+                break;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
